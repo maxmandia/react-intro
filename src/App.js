33d6e99215b69a4946ad6740657cb775b368746f
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-16 16:20:20 -0400

    played around w/ react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xs">
+        <header className="App-header">
+          <ButtonGroup>
+            <Button 
+            startIcon={<SaveIcon />}
+            size='large'
+            variant="contained" 
+            color='primary'>
+            Hello World
+            </Button>
+            <Button 
+            endIcon={<SaveIcon />}
+            size='large'
+            variant="contained" 
+            color='secondary'>
+            Hello World
+            </Button>
+            <Button 
+            endIcon={<SaveIcon />}
+            size='large'
+            variant="contained" 
+            color='secondary'>
+            Hello World
+            </Button>
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </Container>
+      
     </div>
   );
 }
